93d0f90398cfb722226f2365fd8adc0cabb3c5cf
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-04-26 11:34:14 -0400

    begin script, remove function not working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import { choice, remove } from './helpers';
+import foods from './foods';
+
+let items = foods;
+
+console.log(items);
+let randomFruit = choice(items);
+console.log(`I'd like one ${randomFruit}, please.`);
+console.log(`Here you go: ${randomFruit}`);
+items.pop();
+console.log(items);
