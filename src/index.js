20175b8835526f2929fa335d2f04907492ccdb1a
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-04-26 11:40:33 -0400

    finish console script

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import foods from './foods';
 
 let items = foods;
 
-console.log(items);
 let randomFruit = choice(items);
 console.log(`I'd like one ${randomFruit}, please.`);
 console.log(`Here you go: ${randomFruit}`);
+console.log('Delicious! May I have another?');
 remove(items, randomFruit);
-console.log(items.includes(randomFruit));
+console.log(`I'm sorry, we're all out. We have ${items.length} other fruits left.`);
